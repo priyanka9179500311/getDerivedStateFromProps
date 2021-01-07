236159484c673b367db2ce832dc68a6a1cf491a3
priyanka yadav <py631588@gmail.com>
2021-01-07 08:41:31 +0530

    StateFromProps

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
+//import area
+//import something from 'somelibrary';
+
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Create an class Component
+class Oklabs extends Component{
+    //1.Property
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    //2.Constructor
+    constructor(props){
+        //Call the parent Constructor
+        super(props);
+    }
+    //3. Methods
+    //Every class Component Must have render method
+    //Normal Function
+    priyanka(){
+        alert('Its working');
+    }
+    //Fat Arrow Function i.e ES6 Feature
+    dolloracademy = ()=>{
+        alert('Its working Again');
+    }
+    static getDerivedStateFromProps(props,state){
+        return{favoritecolor: props.favcol};
+    }
+    render(){
+        //render method must return HTML (JSX)
+        return(
+            <React.Fragment>
+                <h1>Hello welcome to React </h1>
+                <p> My Favourite Color is { this.state.favouritecolor}</p>
+                <button type="button" onClick={ this.priyanka }> Click Me!</button>
+                <button type="button" onClick={ this.dollaracademy }> Click Me Again !</button>
+            </React.Fragment>
+        );
+
+    }
+}
+//object.method();
+ReactDOM.render(<Oklabs favcol="yellow"/>,document.getElementById('root'))//actual argument
